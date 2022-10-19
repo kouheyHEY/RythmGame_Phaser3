1fc4584833934d7b28f107518201d1fe59ac8870
kouheyHEY <vudtarm@gmail.com>
2022-10-19 23:50:33 +0900

    update

--- js/const/const_assets.js
+++ js/const/const_assets.js
@@ -1,3 +1,6 @@
+// シーン名
+const SCENE_NAME_CHASERGAME = "ChaserGameScene";
+
 // 画像ファイルのディレクトリ
 const DIR_ASSETS = "./assets"
 const DIR_IMG = DIR_ASSETS + "/img"
@@ -7,7 +10,7 @@ const IMG_NAME_XXX = "XXX";
 
 // 画像ファイルロード用配列
 const FILE_NAME_IMG_LIST = {
-    "ChaserScene": {
-        IMG_NAME_XXX: "XXX.png",
+    [SCENE_NAME_CHASERGAME]: {
+        [IMG_NAME_XXX]: "XXX.png",
     }
 };
